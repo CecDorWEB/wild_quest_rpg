4123b9b7626f81e6b902c0470294250663642f95
Damien <150015536+Namy11@users.noreply.github.com>
2024-02-29 16:03:09 +0100

    Animation_Character

--- Assets/Scripts/Character/CharacterMotor.cs
+++ Assets/Scripts/Character/CharacterMotor.cs
@@ -9,15 +9,16 @@ public class CharacterMotor : MonoBehaviour
 
 private InputAction moveAction;
 private GameManager manager;
-
+private Animator anim;
 private Vector2 velocity = Vector2.zero;
+private int Direction = 0;
 [SerializeField] private float speed = 5f;
 
     void Start()
     {
        manager = GameManager.GetInstance(); 
        inputs = manager.GetInputs();
-
+       anim = GetComponent<Animator>();
        moveAction = inputs.actions.FindAction("Move");
     }
 
@@ -25,8 +26,48 @@ private Vector2 velocity = Vector2.zero;
     private void FixedUpdate()
     {
          Vector2 _moveValue = moveAction.ReadValue<Vector2>();
+         _moveValue = ChooseDirection(_moveValue);
+         
          velocity = _moveValue * speed;
 
         transform.position += new Vector3(velocity.x * Time.fixedDeltaTime, velocity.y * Time.fixedDeltaTime, 0);
+
+        anim.SetInteger("Direction", Direction);
+    }
+   private Vector2 ChooseDirection(Vector2 _value)
+    {
+        Vector2 _result = Vector2.zero;
+
+            if (Mathf.Abs(_value.x) >= Mathf.Abs(_value.y))
+        {
+            _result = new Vector2(_value.x,0); 
+        }
+        else
+        {
+            _result = new Vector2(0, _value.y);
+        }
+        Direction = SetDirection(_result);
+        return _result;
+    }
+    private int SetDirection(Vector2 _vector)
+    {
+        if (_vector.x >0)
+        {
+            return 6;
+        }
+        if (_vector.x <0)
+        {
+            return 4;
+        }
+        if (_vector.y >0)
+        {
+            return 8;
+        }
+        if (_vector.y <0)
+        {
+            return 2;
+        }
+
+        return 0;
     }
 }
